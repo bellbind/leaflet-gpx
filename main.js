644161641dd450f5eb6894b75968e5d435f5f075
bellbind <bellbind@gmail.com>
2022-04-11 18:40:35 +0900

    make choice as url

--- main.js
+++ main.js
@@ -4,6 +4,7 @@ const chooser = document.getElementById("chooser");
 chooser.addEventListener("change", ev => {
   if (chooser.selectedIndex === 0) return;
   fetch(chooser.value).then(res => res.text()).then(xml => viewer.setGpx(xml));
+  location.hash = chooser.value;
 });
 
 const options = index.map(gpx => {
@@ -12,3 +13,9 @@ const options = index.map(gpx => {
   return option;
 });
 chooser.append(...options);
+
+const gpx = index.find(({href}) => href === location.hash.replace(/#/, ""));
+if (gpx) {
+  fetch(gpx.href).then(res => res.text()).then(xml => viewer.setGpx(xml));
+  chooser.value = gpx.href;
+}
