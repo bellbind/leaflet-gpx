61556a92f103bfb64c07093e5bad02e2410ecb30
bellbind <bellbind@gmail.com>
2022-04-17 23:16:15 +0900

    introduce gpxPath state for caching

--- main.js
+++ main.js
@@ -1,30 +1,63 @@
+// <leaflet-gpx> with cache
 const viewer = document.getElementById("viewer");
-const index = await (await fetch("./index.json")).json();
-const chooser = document.getElementById("chooser");
-chooser.addEventListener("change", ev => {
-  if (chooser.selectedIndex === 0) return;
-  fetch(chooser.value).then(res => res.text()).then(xml => viewer.setGpx(xml));
-  location.hash = chooser.value;
+const cache = new Map();
+const clear = () => {
+  viewer.clearGpx();
+  viewer.map.setView([0, 0], 0);
+};
+const load = (file) => {
+  if (cache.has(file)) {
+    const gpxPath = cache.get(file);
+    viewer.setGpxPath(gpxPath);
+    return;
+  }
+  fetch(file).then(res => res.text()).then(xml => {
+    const gpxPath = viewer.createGpxPath(xml);
+    viewer.setGpxPath(gpxPath);
+    cache.set(chooser.value, gpxPath);
+  });
+};
+// Event "cursor-changed" from <leaflet-gpx>
+viewer.addEventListener("cursor-changed", ({detail: {cursor, home}}) => {
+  const hash = location.hash.replace(/#/, "");
+  const file = hash.includes("?") ? hash.slice(0, hash.indexOf("?")) : hash;
+  history.replaceState(file, null, `${location.pathname}#${file}?${new URLSearchParams(viewer.getCursor())}`);
 });
 
+// GPX chooser UI
+const chooser = document.getElementById("chooser");
+const index = await (await fetch("./index.json")).json();
 const options = index.map(gpx => {
   const option = document.createElement("option");
   option.textContent = gpx.href;
   return option;
 });
 chooser.append(...options);
-
-viewer.addEventListener("cursor-changed", ({detail: {cursor, home}}) => {
-  const hash = location.hash.replace(/#/, "");
-  const file = hash.includes("?") ? hash.slice(0, hash.indexOf("?")) : hash;
-  location.hash = `${file}?${new URLSearchParams(viewer.getCursor())}`;
+chooser.addEventListener("change", ev => {
+  if (chooser.selectedIndex === 0) {
+    clear();
+    history.replaceState(null, null, location.pathname);
+  } else {
+    load(chooser.value);
+    history.pushState(chooser.value, null, `${location.pathname}#${chooser.value}`);
+  }
 });
 
-const hash = location.hash.replace(/#/, "");
-const file = hash.includes("?") ? hash.slice(0, hash.indexOf("?")) : hash;
-const params = Object.fromEntries(new URLSearchParams(hash.includes("?") ? hash.slice(hash.indexOf("?") + 1) : ""));
-const gpx = index.find(({href}) => href === file);
-if (gpx) {
-  fetch(gpx.href).then(res => res.text()).then(xml => viewer.setGpx(xml).setCursor(params));
-  chooser.value = gpx.href;
-}
+// load location.hash state
+const loadHashState = () => {
+  const hash = location.hash.replace(/#/, "");
+  if (!hash) {
+    clear();
+    return;
+  }
+  const file = hash.includes("?") ? hash.slice(0, hash.indexOf("?")) : hash;
+  const cursor = Object.fromEntries(new URLSearchParams(hash.includes("?") ? hash.slice(hash.indexOf("?") + 1) : ""));
+  const gpx = index.find(({href}) => href === file);
+  if (gpx) {
+    load(gpx.href);
+    viewer.setCursor(cursor);
+    chooser.value = gpx.href;
+  }
+};
+loadHashState();
+window.addEventListener("popstate", loadHashState);
