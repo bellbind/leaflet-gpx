2f783023c7572682d4c8c4286ffc23dabe780ef7
bellbind <bellbind@gmail.com>
2022-08-22 15:42:58 +0900

    add line break in JSON

--- gen-index.deno.js
+++ gen-index.deno.js
@@ -5,4 +5,7 @@ for await (const entry of Deno.readDir("gpx")) {
   if (entry.isFile) data.push({href: `./gpx/${entry.name}`});
 }
 data.sort((a, b) => a.href < b.href ? -1 : a.href.localeCompare(b.href));
-console.log(JSON.stringify(data));
+//console.log(JSON.stringify(data));
+console.log("[");
+console.log(`  ${data.map(entry => JSON.stringify(entry)).join(",\n  ")}`);
+console.log("]");
